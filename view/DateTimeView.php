0c56234a301c1c0635e77b0e4033df248153de1b
Mattias Wikström <mw222rs@student.lnu.se>
2015-09-06 15:18:01 +0200

    datetime fix [deploy: 1dv608]

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -6,16 +6,12 @@ class DateTimeView {
 	public function show() {
 
 		$now = new DateTime("Europe/Stockholm");
-
 		$day = date_format($now, "l");
-
 		$date = date_format($now, "jS");
-
 		$month = date_format($now, "F");
-
+        $year = date_format($now, "Y");
 		$time = date_format($now, "H:i:s");
-
-		$timeString = "$day, the $date of $month, The time is $time";
+		$timeString = "$day, the $date of $month $year, The time is $time";
 
 		return '<p>' . $timeString . '</p>';
 	}

83c6160200f61bd870542415ccc5d9dae51dd1fe
Mattias Wikström <mw222rs@student.lnu.se>
2015-09-06 15:12:17 +0200

    datetime [deploy: 1dv608]

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,17 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$now = new DateTime("GMT+2");
+
+		$day = date_format($now, "l");
+
+		$date = date_format($now, "jS");
+
+		$month = date_format($now, "F");
+
+		$time = date_format($now, "H:i:s");
+
+		$timeString = "$day, the $date of $month, The time is $time";
 
 		return '<p>' . $timeString . '</p>';
 	}

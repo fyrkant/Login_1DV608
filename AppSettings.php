6ed4663b5cf9d5ac963235c55d0578824ce84b05
Mattias Wikström <mw222rs@student.lnu.se>
2015-09-23 09:25:06 +0200

    sample settings help text [deploy: 1dv608]

--- AppSettings.php
+++ AppSettings.php
@@ -3,8 +3,16 @@
 class AppSettings
 {
 
+    /**
+     * Dummy username and password before real register system is implemented.
+     */
     const USERNAME = "Admin";
     const PASSWORD = "Password";
+
+    /**
+     * Data path that defines where files should be saved
+     * (should probably not be a folder reachable for the user).
+     */
     const DATA_PATH = "./../secret/";
 
 }
